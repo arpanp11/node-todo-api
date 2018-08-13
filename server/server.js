c89522e54b8d5ee3ec2609f5a8de6e4c8a05d659
arpanp11 <arpanpatel2810@gmail.com>
2018-08-13 15:45:54 -0500

    Add PATCH todos route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -100,43 +101,90 @@ app.get('/user/:id', (req, res) => {
 //delete todos route
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
-  
+
     if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
-    }
-  
-    Todo.findByIdAndRemove(id).then((todo) => {
-      if (!todo) {
         return res.status(404).send();
-      }
-  
-      res.send(todo);
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
     }).catch((e) => {
-      res.status(400).send();
+        res.status(400).send();
     });
-  });
+});
 
 //delete user route
 app.delete('/user/:id', (req, res) => {
     var id = req.params.id;
-  
+
     if (!ObjectID.isValid(id)) {
-      return res.status(404).send();
-    }
-  
-    Todo.findByIdAndRemove(id).then((doc) => {
-      if (!doc) {
         return res.status(404).send();
-      }
-  
-      res.send(doc);
+    }
+
+    Todo.findByIdAndRemove(id).then((doc) => {
+        if (!doc) {
+            return res.status(404).send();
+        }
+
+        res.send(doc);
     }).catch((e) => {
-      res.status(400).send();
+        res.status(400).send();
     });
-  });
+});
 
 app.listen(port, () => {
     console.log(`Started on port: ${port}.`);
 });
+
+//update todos route
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+//update user route
+// app.patch('/user/:id', (req, res) => {
+//     var id = req.params.id;
+//     var body = _.pick(req.body, ['name', 'kenny']);
+
+//     if (!ObjectID.isValid(id)) {
+//         return res.status(404).send();
+//     }
+
+//     Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((doc) => {
+//         if (!doc) {
+//             return res.status(404).send();
+//         }
+
+//         res.send({ doc });
+//     }).catch((e) => {
+//         res.status(400).send();
+//     })
+// });
 
 module.exports = { app };

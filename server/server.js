e49d5f1623b29f1a8a86fdaa666f716bd7ca7cde
arpanp11 <arpanpatel2810@gmail.com>
2018-08-13 11:44:17 -0500

    Add DELETE todos/:id and user?:id routes

--- server/server.js
+++ server/server.js
@@ -97,6 +97,44 @@ app.get('/user/:id', (req, res) => {
     });
 });
 
+//delete todos route
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+  
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    Todo.findByIdAndRemove(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+  
+      res.send(todo);
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  });
+
+//delete user route
+app.delete('/user/:id', (req, res) => {
+    var id = req.params.id;
+  
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+  
+    Todo.findByIdAndRemove(id).then((doc) => {
+      if (!doc) {
+        return res.status(404).send();
+      }
+  
+      res.send(doc);
+    }).catch((e) => {
+      res.status(400).send();
+    });
+  });
+  
 app.listen(port, () => {
     console.log(`Started on port ${port}.`);
 });

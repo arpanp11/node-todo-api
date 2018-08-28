8a43799fc041c66f0dfe4b0cc5cf2ea9bb1156c9
Arpan Patel <arpanpatel@Arpans-MacBook-Pro.local>
2018-08-28 19:38:35 -0400

    Add hashing password with bcrypt

--- local-test/hashing.js
+++ local-test/hashing.js
@@ -1,12 +1,26 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 15
-};
+var password = 'hello$dj';
 
-var token  = jwt.sign(data,'dec16');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decode = jwt.verify(token, 'dec16');
-console.log(decode);
+var hashPass = '$2a$10$vy8Emnluj.WbgDsYJPYXaeGc9bYTnYhoel.XBJxxYEn.Eqb5KDgVu';
+
+bcrypt.compare(password, hashPass, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 15
+// };
+
+// var token  = jwt.sign(data,'dec16');
+// console.log(token);
+
+// var decode = jwt.verify(token, 'dec16');
+// console.log(decode);

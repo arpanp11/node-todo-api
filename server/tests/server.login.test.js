bd728a08582b4b0f2f141d5fc18ad71315384b81
Arpan Patel <arpanpatel@Arpans-MacBook-Pro.local>
2018-09-09 21:50:00 -0400

    Add test for POST /login/signin route

--- server/tests/server.login.test.js
+++ server/tests/server.login.test.js
@@ -56,7 +56,7 @@ describe('POST /login', () => {
                     expect(user).toExist();
                     expect(user).toNotBe(password);
                     done();
-                });
+                }).catch((e)=> done(e));
             });
     });
 
@@ -80,5 +80,57 @@ describe('POST /login', () => {
             })
             .expect(400)
             .end(done);
+    });
+});
+
+//PODT /login/signin route test
+describe('POST /login/singin', () => {
+    it('should singin user and return auth token', (done) => {
+        request(app)
+            .post('/login/singin')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err);
+                }
+
+                Login.findById(users[1]._id).then((user)=>{
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e)=> done(e));
+            });
+    });
+
+    it('should reject invalid singin', (done) => {
+        request(app)
+        .post('/login/singin')
+        .send({
+            email: users[1].email,
+            password: users[1].password + '1'
+        })
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toNotExist();
+        })
+        .end((err, res)=>{
+            if(err){
+                return done(err);
+            }
+
+            Login.findById(users[1]._id).then((user)=>{
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e)=> done(e));
+        });
     });
 });
